3329e81f0f000081658f759fae11d4d01ea0039b
waqarahmadm019 <waqarahmadm019@users.noreply.github.com>
2017-05-02 16:12:42 +0500

    Testing Mazaq Raat

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World Atlassian Bamboo configuration checking Hurrah</p>'; ?> 
+ <?php echo '<p>Mazaq Raat Hello World Atlassian Bamboo configuration checking Hurrah</p>'; ?> 
  </body>
 </html>
