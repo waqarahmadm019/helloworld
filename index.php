a9776124bfd7cae0ce7a50536542cec440a64474
waqarahmadm019 <waqarahmadm019@users.noreply.github.com>
2017-05-02 16:30:07 +0500

    Contents removed.

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Mazaq Raat Hello World Atlassian Bamboo configuration checking Hurrah</p>'; ?> 
+ <?php echo '<p>Hello World Atlassian Bamboo configurations</p>'; ?> 
  </body>
 </html>
