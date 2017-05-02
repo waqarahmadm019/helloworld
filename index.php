e0793bfdbcb6ec736c59fb62ed231a46c26cb1fb
waqarahmadm019 <waqarahmadm019@users.noreply.github.com>
2017-05-02 16:07:02 +0500

    Checking...

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World Atlassian Bamboo configuration checking</p>'; ?> 
+ <?php echo '<p>Hello World Atlassian Bamboo configuration checking Hurrah</p>'; ?> 
  </body>
 </html>
