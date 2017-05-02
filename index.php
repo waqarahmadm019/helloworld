d4b47d72e2225b472c04bb8612409ea644a63067
waqarahmadm019 <waqarahmadm019@users.noreply.github.com>
2017-05-02 16:33:43 +0500

    change

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World Atlassian Bamboo configurations</p>'; ?> 
+ <?php echo '<p>Yar ko ham ny ja baja daikha. Hello World Atlassian Bamboo configurations</p>'; ?> 
  </body>
 </html>
