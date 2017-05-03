d5283f304f894c8ac6d9d9e14c408c1464cd3227
waqarahmadm019 <waqarahmadm019@users.noreply.github.com>
2017-05-03 15:58:47 +0500

    dev test

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World Atlassian Bamboo configurations.</p>'; ?> 
+ <?php echo '<p>Dev Test. Hello World Atlassian Bamboo configurations.</p>'; ?> 
  </body>
 </html>
