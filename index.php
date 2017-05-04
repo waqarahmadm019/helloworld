a825df66be02bf51d208e365d5646dd40f437072
waqarahmadm019 <waqarahmadm019@users.noreply.github.com>
2017-05-04 15:18:19 +0500

    content added

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
  </head>
  <body>
  <?php echo '<p>Dev Test. Checking Bamboo dev plan configurations.</p>'; ?> 
+  DEV. Hello World Atlassian Bamboo configurations
  </body>
 </html>
 Hello World Atlassian Bamboo configurations
