2c7e769337c738fb57643fd2ec6ea25bd2b39859
waqarahmadm019 <waqarahmadm019@users.noreply.github.com>
2017-05-04 15:14:11 +0500

    dev bamboo configuration checking

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Checking Master Plan.Hello World Atlassian Bamboo configurations</p>'; ?> 
+ <?php echo '<p>helloworld project created just for checking bamboo configurations.</p>'; ?> 
  </body>
 </html>
+Checking Master Plan.Hello World Atlassian Bamboo configurations
